5234d31b2e588f68c7caee4f8617943d08cfef56
leonardoconsutec <leonardo.lopes@consutec.net.br>
2017-02-22 15:47:00 -0300

    Calculo do Fatorial de 4 em vez de 0

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ public class TesteFatorial{
    //Comentario inserido em TesteFatorial.java
    public static void main(String[] args){
        Fatorial fat = new Fatorial();
-       int resultado = fat.calcularFatorialIterativo(0);
+       int resultado = fat.calcularFatorialIterativo(4);
        System.out.println("Resultado: "+resultado);
    }
 }
