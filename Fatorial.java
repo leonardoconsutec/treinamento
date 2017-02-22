3e3590118321c880076f7dd2f58a08f9812bf427
leonardoconsutec <leonardo.lopes@consutec.net.br>
2017-02-22 13:55:17 -0300

    Alteracao para o calculo correto do fatorial de zero

--- Fatorial.java
+++ Fatorial.java
@@ -4,9 +4,11 @@ public class Fatorial{
      if(n < 0){
        //erro menor que zero
      }
-     int resultado = 1;
-     for(int i = 2;i <= n ; i++){
-       resultado*=i;
+     if(n != 0){
+       int resultado = 1;
+       for(int i = 2;i <= n ; i++){
+         resultado*=i;
+       }
      }
      return resultado;
   }
