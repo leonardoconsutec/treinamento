46fe52f0b4704431e927772f8cefd9c72d37ecd5
leonardoconsutec <leonardo.lopes@consutec.net.br>
2017-02-22 13:40:29 -0300

    alteracao para lnacar execessao para numeros negativos

--- Fatorial.java
+++ Fatorial.java
@@ -1,6 +1,9 @@
 public class Fatorial{
   //versao iterativa do calculo fatorial
   public int calcularFatorial(int n){
+     if(n < 0){
+       //erro menor que zero
+     }
      int resultado = 1;
      for(int i = 2;i <= n ; i++){
        resultado*=i;
