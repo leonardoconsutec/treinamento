dd6acbe0843ab02bfcff7c11aeef6bb130bb5ab6
leonardoconsutec <leonardo.lopes@consutec.net.br>
2017-02-22 15:48:28 -0300

    Calculo fatorial de 10 e renomeacao da variavel local

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ public class TesteFatorial{
    //Comentario inserido em TesteFatorial.java
    public static void main(String[] args){
        Fatorial fat = new Fatorial();
-       int resultado = fat.calcularFatorialIterativo(0);
-       System.out.println("Resultado: "+resultado);
+       int r = fat.calcularFatorialIterativo(10);
+       System.out.println("Resultado: "+r);
    }
 }
