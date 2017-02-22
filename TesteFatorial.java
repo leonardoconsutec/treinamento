c0f37feb52adb5590bfdb01500bd03c41bd131e6
leonardoconsutec <leonardo.lopes@consutec.net.br>
2017-02-22 15:35:39 -0300

    Comentario inserido em TesteFatorial.java

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,4 +1,5 @@
 public class TesteFatorial{
+   //Comentario inserido em TesteFatorial.java
    public static void main(String[] args){
        Fatorial fat = new Fatorial();
        int resultado = fat.calcularFatorialIterativo(0);
