8020251ebb67acaf1759a154f194927848421382
leonardoconsutec <leonardo.lopes@consutec.net.br>
2017-03-08 08:25:15 -0300

    Fatorial recursivo implementado

---
+++ FatorialRecursivo.java
@@ -0,0 +1,9 @@
+public class FatorialRecursivo{
+    public static int calcular(){
+      int teste = 0;
+      return teste;
+    }
+    public static void main (String[] args){
+         System.out.println(calcular());
+    }
+}
